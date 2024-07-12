c88394ce303c02c96c3dfc159130337e78bbab52
C-yoshida618 <kanamakihira@gmail.com>
2024-07-12 21:49:27 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true, length: { maximum: 50 }
+  validates :phone_number, presence: true, numericality: true, length: { is: 11 }
+  validates :address, presence: true, length: { maximum: 255 }
 end
