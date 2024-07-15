ad541126cdedf76bec5582eeb01b8f61bc8f7089
C-yoshida618 <kanamakihira@gmail.com>
2024-07-15 11:11:18 +0900

    アクティブハッシュとのアソシエーションとバリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -12,4 +12,22 @@ class Item < ApplicationRecord
 
   # アクティブハッシュとのアソシエーション
   belongs_to :category
+  belongs_to :item_status
+  belongs_to :shipping_cost
+  belongs_to :shipping_area
+  belongs_to :shipping_day
+
+  with_options presence: true do
+    validates :image
+    validates :item_name
+    validates :description
+    validates :user
+    validates :category_id
+    validates :status_id
+    validates :shipping_cost_id
+    validates :shipping_area_id
+    validates :shipping_day_id
+    # 300円以上かつ9,999,999円以下で、半角数字でないと入力不可
+    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+  end
 end
