e4ad028f70e5438e351a359cd1c898fc03aec7ac
C-yoshida618 <kanamakihira@gmail.com>
2024-07-16 20:02:51 +0900

    # 重複処理をまとめる

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,9 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
 
+  # 重複処理をまとめる
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
+
   def index
     # @items = Item.includes(:user).order('created_at DESC')
   end
