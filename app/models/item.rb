a52162af280239e8b43e8ac47eee616d76524494
C-yoshida618 <kanamakihira@gmail.com>
2024-07-16 14:21:36 +0900

    レビュー後修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -19,14 +19,13 @@ class Item < ApplicationRecord
     validates :image
     validates :item_name, length: { maximum: 40 }
     validates :description, length: { maximum: 1000 }
-    validates :user_id
     validates :category_id
     validates :item_status_id
     validates :shipping_cost_id
     validates :shipping_area_id
     validates :shipping_day_id
     # 300円以上かつ9,999,999円以下で、半角数字でないと入力不可
-    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+    validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
   end
 
   # ジャンルの選択が「--」の時は保存不可
