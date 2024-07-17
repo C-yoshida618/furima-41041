8e8844ab8217830511489cafbbc9b6082f2e5896
C-yoshida618 <kanamakihira@gmail.com>
2024-07-17 17:15:55 +0900

    updateアクションの定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -33,6 +33,15 @@ class ItemsController < ApplicationController
     redirect_to root_path
   end
 
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path
+    else
+      render :edit
+    end
+  end
+
   private
 
   def item_params
