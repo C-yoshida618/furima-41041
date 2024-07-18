f88ec4a515bbe06c7ac6df2e17b52c50dfc1bfbe
C-yoshida618 <kanamakihira@gmail.com>
2024-07-18 18:22:27 +0900

    決済処理実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -6,19 +6,22 @@ class OrdersController < ApplicationController
   def create
     @orderform = OrderForm.new(order_params)
     if @orderform.valid?
-      # pay_item
+      Payjp.api_key = 'sk_test_57b3fceee71debe1e88b5d5a' # 自身のPAY.JPテスト秘密鍵を記述しましょう
+      Payjp::Charge.create(
+        amount: order_params[:price],  # 商品の値段
+        card: order_params[:token],    # カードトークン
+        currency: 'jpy' # 通貨の種類（日本円）
+      )
       @orderform.save(params, current_user.id)
       redirect_to root_path
     else
-      render :index
+      render :index, status: :unprocessable_entity
     end
   end
 
   private
 
   def order_params
-    params.require(:order_form).permit(:postal_code, :prefecture_id, :city, :street_address, :building_name, :phone_number).merge(
-      item_id: params[:item_id], user_id: current_user.id
-    )
+    params.require(:orderform).permit(:price).merge(token: params[:token])
   end
 end
