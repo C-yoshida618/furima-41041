31229ed10a216fc14bde77fdb6da5b945a65bd8e
C-yoshida618 <kanamakihira@gmail.com>
2024-07-20 20:00:19 +0900

    テストコード実装

--- app/models/orders_purchases.rb
+++ app/models/orders_purchases.rb
@@ -2,13 +2,17 @@ class OrdersPurchases
   include ActiveModel::Model
   attr_accessor :token, :price, :item_id, :user_id, :postal_code, :shipping_area_id, :city, :block, :building, :phone_number
 
-  validates :shipping_area_id, numericality: { other_than: 0 }
+  validates :shipping_area_id, numericality: { other_than: 0, message: "can't be blank" }, presence: true
   validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/ }
   validates :item_id, presence: true
   validates :phone_number, format: { with: /\A\d{10,11}\z/ }, presence: true
   validates :user_id, presence: true
   validates :city, presence: true
-  validates :building, presence: true
+  validates :block, presence: true
+  validates :token, presence: true
+
+  # 建物名は任意
+  validates :building, absence: false
 
   def save
     order = Order.create(user_id:, item_id:)
--- spec/models/orders_purchases_spec.rb
+++ spec/models/orders_purchases_spec.rb
@@ -2,81 +2,84 @@ require 'rails_helper'
 
 RSpec.describe OrdersPurchases, type: :model do
   before do
-    @user = FactoryBot.create(:user)
-    @item = FactoryBot.create(:product)
-    @OrdersPurchases = FactoryBot.build(:purchase_address, user_id: @user.id, item_id: @item.id) # rubocop:disable Naming/VariableName
+    user = FactoryBot.create(:user)
+    item = FactoryBot.create(:item)
+    @orders_purchases = FactoryBot.build(:orders_purchases, user_id: user.id, item_id: item.id)
   end
 
   context '内容に問題ない場合' do
     it 'すべての値が正しく入力されていれば購入できること' do
-      expect(@OrdersPurchases).to be_valid
+      expect(@orders_purchases).to be_valid
     end
+
     it '建物名が空でも購入できること' do
-      @OrdersPurchases.building_name = ''
-      expect(@OrdersPurchases).to be_valid
+      @orders_purchases.building = ''
+      expect(@orders_purchases).to be_valid
     end
   end
 
   context '内容に問題がある場合' do
     it '郵便番号は空では保存できないこと' do
-      @OrdersPurchases.postal_code = ''
-      @OrdersPurchases.valid?
-      expect(@OrdersPurchases.errors.full_messages).to include("Post code can't be blank")
+      @orders_purchases.postal_code = ''
+      @orders_purchases.valid?
+      expect(@orders_purchases.errors.full_messages).to include('Postal code is invalid')
     end
-    it '郵便番号は『３桁ハイフン４桁』半角英数字でないと保存できないこと' do
-      @OrdersPurchases.postal_code = '123-123４'
-      @OrdersPurchases.valid?
-      expect(@OrdersPurchases.errors.full_messages).to include('Post code is invalid. Include hyphen(-)')
+
+    it '郵便番号は「3桁ハイフン4桁」の半角数字でなければ保存できないこと' do
+      @orders_purchases.postal_code = '1234567'
+      @orders_purchases.valid?
+      expect(@orders_purchases.errors.full_messages).to include('Postal code is invalid')
     end
+
     it '都道府県に「---」が選択されている場合は購入できないこと' do
-      @OrdersPurchases.shipping_area_id = '1'
-      @OrdersPurchases.valid?
-      expect(@OrdersPurchases.errors.full_messages).to include("Prefecture can't be blank")
+      @orders_purchases.shipping_area_id = '0'
+      @orders_purchases.valid?
+      expect(@orders_purchases.errors.full_messages).to include("Shipping area can't be blank")
     end
     it '市区町村が空だと購入できないこと' do
-      @OrdersPurchases.city = ''
-      @OrdersPurchases.valid?
-      expect(@OrdersPurchases.errors.full_messages).to include("City can't be blank")
+      @orders_purchases.city = ''
+      @orders_purchases.valid?
+      expect(@orders_purchases.errors.full_messages).to include("City can't be blank")
     end
     it '番地が空だと購入できないこと' do
-      @OrdersPurchases.block = ''
-      @OrdersPurchases.valid?
-      expect(@OrdersPurchases.errors.full_messages).to include("Address can't be blank")
+      @orders_purchases.block = ''
+      @orders_purchases.valid?
+      expect(@orders_purchases.errors.full_messages).to include("Block can't be blank")
     end
     it '電話番号が空だと購入できないこと' do
-      @OrdersPurchases.phone_number = nil
-      @OrdersPurchases.valid?
-      expect(@OrdersPurchases.errors.full_messages).to include("Phone number can't be blank")
+      @orders_purchases.phone_number = nil
+      @orders_purchases.valid?
+      expect(@orders_purchases.errors.full_messages).to include("Phone number can't be blank")
     end
     it '電話番号が9桁以下だと購入できないこと' do
-      @OrdersPurchases.phone_number = '090123456'
-      @OrdersPurchases.valid?
-      expect(@OrdersPurchases.errors.full_messages).to include('Phone number is invalid')
+      @orders_purchases.phone_number = '090123456'
+      @orders_purchases.valid?
+      expect(@orders_purchases.errors.full_messages).to include('Phone number is invalid')
     end
     it '電話番号が12桁以上だと購入できない' do
-      @OrdersPurchases.phone_number = '090123456789'
-      @OrdersPurchases.valid?
-      expect(@OrdersPurchases.errors.full_messages).to include('Phone number is invalid')
+      @orders_purchases.phone_number = '090123456789'
+      @orders_purchases.valid?
+      expect(@orders_purchases.errors.full_messages).to include('Phone number is invalid')
     end
     it '電話番号が半角数値でないと購入できないこと' do
-      @OrdersPurchases.phone_number = '０9012341234'
-      @OrdersPurchases.valid?
-      expect(@OrdersPurchases.errors.full_messages).to include('Phone number is invalid')
+      @orders_purchases.phone_number = '０9012341234'
+      @orders_purchases.valid?
+      expect(@orders_purchases.errors.full_messages).to include('Phone number is invalid')
     end
     it 'tokenが空では購入できないこと' do
-      @OrdersPurchases.token = nil
-      @OrdersPurchases.valid?
-      expect(@OrdersPurchases.errors.full_messages).to include("Token can't be blank")
+      @orders_purchases.token = nil
+      @orders_purchases.valid?
+      expect(@orders_purchases.errors.full_messages).to include("Token can't be blank")
     end
     it 'user_idが紐づいていなければ購入できないこと' do
-      @OrdersPurchases.user_id = ''
-      @OrdersPurchases.valid?
-      expect(@OrdersPurchases.errors.full_messages).to include("User can't be blank")
+      @orders_purchases.user_id = ''
+      @orders_purchases.valid?
+      expect(@orders_purchases.errors.full_messages).to include("User can't be blank")
     end
     it 'item_idが紐づいていなければ購入できないこと' do
-      @OrdersPurchases.item_id = ''
-      @OrdersPurchases.valid?
-      expect(@OrdersPurchases.errors.full_messages).to include("Product can't be blank")
+      @orders_purchases.item_id = ''
+      @orders_purchases.valid?
+      expect(@orders_purchases.errors.full_messages).to include("Item can't be blank")
     end
   end
 end
