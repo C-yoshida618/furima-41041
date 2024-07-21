c3a2d467f6fb31f746ab3f67b8f32954fd00a2f9
C-yoshida618 <kanamakihira@gmail.com>
2024-07-21 14:49:06 +0900

    LGTM後の調整

--- app/models/orders_purchases.rb
+++ app/models/orders_purchases.rb
@@ -1,6 +1,6 @@
 class OrdersPurchases
   include ActiveModel::Model
-  attr_accessor :token, :item_id, :user_id, :postal_code, :shipping_area_id, :city, :block, :building, :phone_number
+  attr_accessor :price, :token, :item_id, :user_id, :postal_code, :shipping_area_id, :city, :block, :building, :phone_number
 
   validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/ }
 
@@ -10,7 +10,7 @@ class OrdersPurchases
     validates :phone_number, format: { with: /\A\d{10,11}\z/ }
     validates :user_id
     validates :city
-    validates :Block
+    validates :block
     validates :token
   end
   # 建物名は任意
@@ -18,7 +18,7 @@ class OrdersPurchases
 
   def save
     order = Order.create(user_id:, item_id:)
-    Address.create(postal_code:, shipping_area_id:, city:, block:, building:,
-                   phone_number:, order_id: order.id)
+    Purchase.create(postal_code:, shipping_area_id:, city:, block:, building:,
+                    phone_number:, order_id: order.id)
   end
 end
